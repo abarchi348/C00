eee45bc6e7081accc85a27fbf216f19e47a01db6
Abarchi <Oups@MISTER-BARKE.local>
2025-05-05 16:41:41 +0100

    first commit

--- check_file.sh
+++ check_file.sh
@@ -5,8 +5,8 @@ read -p "Entrez le nom du fichier à vérifier : " filename
 
 # Vérifie si le fichier existe
 if [ -f "$filename" ]; then
-  echo " Le fichier '$filename' existe."
+  echo "✅ Le fichier '$filename' existe."
 else
-  echo " Le fichier '$filename' n'existe pas."
+  echo "❌ Le fichier '$filename' n'existe pas."
 fi
 

b271c7c7e1d6ccfd16ae2f76d24abd83bc5d779d
Abarchi <Oups@MISTER-BARKE.local>
2025-05-06 00:11:32 +0100

    first commit

--- create_directory.sh
+++ create_directory.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# Ce script crée un répertoire nommé 'test_directory'
-mkdir test_directory
+# Crée un dossier test_directory et gère les erreurs
+
+if mkdir test_directory 2>/dev/null; then
+  echo "Le répertoire 'test_directory' a été créé."
+else
+  echo "Erreur : le répertoire existe déjà ou n’a pas pu être créé."
+fi

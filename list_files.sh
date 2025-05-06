25ca8521748592d0c214457115cbf004c7f90af4
Abarchi <Oups@MISTER-BARKE.local>
2025-05-06 15:32:03 +0100

    first commit

--- list_files.sh
+++ list_files.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Liste uniquement les fichiers du répertoire courant
-if [ -r . ]; then
-  ls -p | grep -v /
-else
-  echo "Erreur : impossible d’accéder au répertoire courant."
-fi
+# Ce script affiche le contenu du répertoire courant
+ls

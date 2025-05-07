4389b936643e786d5fa95c47ec2c7e05f83a43ac
Abarchi <Oups@MISTER-BARKE.local>
2025-05-07 12:21:06 +0100

    first commit

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,6 +1,6 @@
   #!/bin/bash
-  echo "Entrez le nom du fichier : "
-  read   filename
+  echo "Entrez le nom du fichier :"
+  read filename
 
   if [ -f "$filename" ]; then
     echo "Le fichier '$filename' existe."
